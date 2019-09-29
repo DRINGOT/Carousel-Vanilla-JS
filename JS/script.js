bb2a9171631aea4145c671fcf5c46fe929ed8d58
DRINGOT <donovan.ringot@hotmail.fr>
2019-09-29 18:40:51 +0200

    declare variables in script.js

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const next = document.querySelector('.next')
+const previous = document.querySelector('.previous')
+const images = document.querySelectorAll('li');
+const button = document.querySelector('button');
+let i = 0;

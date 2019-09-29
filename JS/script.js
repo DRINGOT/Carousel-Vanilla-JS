9ae1ec44d49197b079f1ffaf04d93b41e556f29d
DRINGOT <donovan.ringot@hotmail.fr>
2019-09-29 18:42:15 +0200

    create function scrollingNext to pass at the next li in script.js

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,11 @@ const previous = document.querySelector('.previous')
 const images = document.querySelectorAll('li');
 const button = document.querySelector('button');
 let i = 0;
+
+function scrollingNext() {
+    images[i].classList.remove('active'); i++;
+    if (i >= document.images.length) {
+        i = 0
+    }
+    images[i].classList.add('active');
+}
